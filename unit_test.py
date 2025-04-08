3c16c94018464c2476e43588f4c73ee20104f612
lain-lane <eorr2@jh.edu>
2025-04-08 00:30:57 -0400

    Add files via upload

--- unit_test.py
+++ unit_test.py
@@ -24,6 +24,19 @@ class TestPosCheck(unittest.TestCase):
         self.assertFalse(solver.pos_check((5,1),grid))
         # testing upper limit in x
 
+class TestGetOpens(unittest.TestCase):
+    def test_1(self):
+        grid=np.array([['o','o']])
+        self.assertTrue(len(solver.get_open(grid))==2)
+        # testing that it finds the right number of opens
+
+    def test_2(self):
+        grid=np.zeros((3,3),dtype=str)
+        grid[1,2]='o'
+        space=solver.get_open(grid)[0]
+        self.assertTrue(space[0]==2 and space[1]==1)
+        # testing that it gets the xy values correct
+
 
 if __name__=='__main__':
     unittest.main()
